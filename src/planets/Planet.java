2e5ef01e127c620bc3fe8a6a0a67202badd054f7
Francis Nixon <13nixonf@gmail.com>
2016-03-09 20:17:57 +0000

    more stuff

---
+++ src/planets/Planet.java
@@ -0,0 +1,15 @@
+package universe;
+
+/**
+ * Created by bob on 3/5/2016.
+ */
+
+/*planet contains the following:
+    grid array that serves as building block of planet
+    resources on each grid array
+    possible hazards volcano,temperature changes,weather. hazards are local to grid array
+
+
+*/
+public class Planet {
+}
