c73ed829a5867e0e13a065765c1aa3194f777361
Francis Nixon <13nixonf@gmail.com>
2016-03-28 23:50:36 +0000

    stopping now

---
+++ src/people/cityworkers/Bureaucrat.java
@@ -0,0 +1,4 @@
+public class Bureaucrat extends CityWorker
+{
+	
+}
--- src/science/Discovery.java
+++ src/science/Discovery.java
@@ -1,5 +1,17 @@
 public abstract class Discovery//maybe make interface??
 {
-	public double percentComplete; //0 to 1
+	private double percentComplete = 0.0; //0 to 1
 	public abstract void postDiscovery();
+	public void makeProgress(double progress)
+	{
+		percentComplete += progress;
+		if(percentComplete > 1.0)
+			postDiscovery();
+	}
+	public boolean discoveredQ()
+	{
+		if(percentComplete > 1.0)
+			return true;
+		return false;
+	}
 }
